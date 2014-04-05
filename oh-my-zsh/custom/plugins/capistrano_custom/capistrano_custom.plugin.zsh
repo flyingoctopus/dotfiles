e526adfbfc5d9fdcd57f45fe63d180da942e022d
Pablo Cantero <pablo@pablocantero.com>
2014-04-04 21:16:06 -0300

    Uses `shipit` instead of `xcap`

--- oh-my-zsh/custom/plugins/capistrano_custom/capistrano_custom.plugin.zsh
+++ oh-my-zsh/custom/plugins/capistrano_custom/capistrano_custom.plugin.zsh
@@ -1,8 +1,8 @@
-# Added `xcap` because `cap` is a reserved word. `cap` completion doesn't work.
+# Added `shipit` because `cap` is a reserved word. `cap` completion doesn't work.
 # https://stackoverflow.com/questions/21353937/is-cap-a-reserved-word-zsh-completion?noredirect=1#comment32196838_21353937
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Modules.html#The-zsh_002fcap-Module
 
-func xcap() {
+func shipit() {
   if [ -f Gemfile ]
   then
     bundle exec cap $*
